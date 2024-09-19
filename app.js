01653f1d1ed5747c199a3bf175afb505a844300e
jrukundo256 <rukundojonathan360@gmail.com>
2024-09-19 19:28:24 +0300

    Set up initial project structure..

--- app.js
+++ app.js
@@ -2,25 +2,25 @@ const express = require('express');
 const morgan = require('morgan');
 const fs = require('fs');
 const path = require('path');
-const quoteRoutes = require('./routes/quoteRoutes');
-const authorRoutes = require('./routes/authorRoutes');
-const authRoutes = require('./routes/authRoutes');
-const { authenticateJWT } = require('./middleware/authMiddleware');
+// const quoteRoutes = require('./routes/quoteRoutes');
+// const authorRoutes = require('./routes/authorRoutes');
+// const authRoutes = require('./routes/authRoutes');
+// const { authenticateJWT } = require('./middleware/authMiddleware');
 
 const app = express();
 
 // M/W
 app.use(express.json());
-app.use(morgan('combined', {
-  stream: fs.createWriteStream(path.join(__dirname, 'logs', 'request_logs.txt'), { flags: 'a' })
-}));
+// app.use(morgan('combined', {
+//   stream: fs.createWriteStream(path.join(__dirname, 'logs', 'request_logs.txt'), { flags: 'a' })
+// }));
 
 // Public routes
-app.use('/auth', authRoutes);
+// app.use('/auth', authRoutes);
 
 // Protected routes
-app.use('/quotes', authenticateJWT, quoteRoutes);
-app.use('/authors', authenticateJWT, authorRoutes);
+// app.use('/quotes', authenticateJWT, quoteRoutes);
+// app.use('/authors', authenticateJWT, authorRoutes);
 
 const PORT = 3500;
 
